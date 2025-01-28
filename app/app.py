cddccb714690eea0401295c39fc0bfad7a95aca8
Paras <admin@syntax-ai.com>
2025-01-28 12:50:20 -0500

    added dockerfile + Register class

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+from pymongo import MongoClient
 from sentence_transformers import SentenceTransformer
+import bcrypt
 
 
 
@@ -9,8 +12,52 @@ app = Flask(__name__)
 api = Api(app)
 
 
-# 
+#db setup
+client = MongoClient("mongodb://db:27017")
+db = client.SentencesDatabase
+users = db['Users']
 
+# ***********************************************
+# Register users
+class Register(Resource):
+    def post(self):
+        # get posted data by the user
+        postedData = request.get_json()
+        
+        #data
+        username = postedData["username"]
+        password = postedData["password"]
+        
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        users.insert_one({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": "",
+            "Tokens": 6
+        })
+        
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }    
+        return jsonify(retJson)
+    
+    def verifyPw(username, password):
+        hashed_pw = users.find({
+            "Username": username
+        })[0]["Password"]
+        
+        if bcrypt.hashpw(password.encode('utf-8'), hashed_pw) == hashed_pw:
+            return True
+        else:
+            return False
+        
+    def countTokens(username):
+        tokens = users.find({
+            "Username": username
+        })[0]["Tokens"]
+        return tokens
 
 
 # ************************************************ 
